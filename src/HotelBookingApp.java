5020543eb134db13bdbd12f8fc386c16fa6f00d8
Madhuri-Annapureddy <maadhurie07@gmail.com>
2026-04-10 09:42:33 +0530

    Base application setup

---
+++ src/HotelBookingApp.java
@@ -0,0 +1,7 @@
+public class HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("   Welcome to Book My Stay App");
+        System.out.println("   Hotel Booking Management System");
+        System.out.println("   Version: 1.0");
+    }
+}
